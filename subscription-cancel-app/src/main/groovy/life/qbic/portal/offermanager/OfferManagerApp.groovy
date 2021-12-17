9e97123eb506d5901129a19bc34ae60aec6a0599
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2021-12-17 09:21:37 +0100

    init

---
+++ subscription-cancel-app/src/main/groovy/life/qbic/portal/offermanager/OfferManagerApp.groovy
@@ -0,0 +1,50 @@
+package life.qbic.portal.offermanager
+
+import com.vaadin.annotations.Theme
+import com.vaadin.server.Page
+import com.vaadin.server.VaadinRequest
+import com.vaadin.ui.Layout
+import com.vaadin.ui.Notification
+import com.vaadin.ui.VerticalLayout
+import groovy.transform.CompileStatic
+import groovy.util.logging.Log4j2
+
+/**
+ * Entry point for the application. This class derives from {@link life.qbic.portal.portlet.QBiCPortletUI}.
+ *
+ * @since: 1.0.0
+ * @author: Jennifer Bödker
+ * @see <a href=https://github.com/qbicsoftware/portal-utils-lib>portal-utils-lib</a>
+ */
+@Theme("mytheme")
+@SuppressWarnings("serial")
+@Log4j2
+@CompileStatic
+class OfferManagerApp extends QBiCPortletUI {
+
+    private DependencyManager dependencyManager
+
+    OfferManagerApp() {
+        super()
+        // The constructor MUST NOT fail since the user does not get any feedback otherwise.
+        try {
+            create()
+        } catch (Exception e) {
+            log.error("Could not initialize {}", OfferManagerApp.getCanonicalName(), e)
+        } catch (Error error) {
+            log.error("Unexpected runtime error.", error)
+        }
+    }
+
+    private void create() {
+        this.dependencyManager = new DependencyManager()
+    }
+
+    @Override
+    protected Layout getPortletContent(final VaadinRequest request) {
+        def layout = new VerticalLayout()
+
+        return layout
+    }
+
+}
