b7e562f997c1decde8156ac38a131d9707c3dc37
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2021-12-21 10:03:50 +0100

    Address request changes

--- subscription-cancel-app/src/main/groovy/life/qbic/portal/subscription/view/SubscriptionInterface.groovy
+++ subscription-cancel-app/src/main/groovy/life/qbic/portal/subscription/view/SubscriptionInterface.groovy
@@ -7,27 +7,38 @@ import com.vaadin.ui.Label
 import com.vaadin.ui.Layout
 import com.vaadin.ui.VerticalLayout
 import com.vaadin.ui.themes.ValoTheme
+import life.qbic.portal.subscription.view.model.ConfirmationDisplay
+import life.qbic.portal.subscription.view.model.FailureDisplay
 import life.qbic.portal.subscription.view.model.SubscriptionModel
 
 /**
- * <class short description - 1 Line!>
+ * <b>Class SubscriptionInterface</b>
  *
- * <More detailed description - When to use, what it solves, etc.>
+ * <p>The interface, displaying the subscription cancellation confirmation to the user.</p>
  *
- * @since <version tag>
+ * @since 1.0.0
  */
 class SubscriptionInterface extends VerticalLayout {
 
+    /**
+     * The interface data model
+     */
     SubscriptionModel model
 
-    Label project
+    /**
+     * The screen that is displayed upon successful subscription cancellation
+     */
+    ConfirmationDisplay confirmationDisplay
 
-    Label email
+    /**
+     * The screen that is displayed upon cancellation failure
+     */
+    FailureDisplay failureDisplay
 
-    Layout confirmation
-
-    Layout failureNotification
-
+    /**
+     * The constructor to use with its data model passed as argument.
+     * @param model the interface data model
+     */
     SubscriptionInterface(SubscriptionModel model) {
         this.model = model
         init()
@@ -35,94 +46,37 @@ class SubscriptionInterface extends VerticalLayout {
         setListeners()
     }
 
-    void bindToModel() {
-        project.setValue(model.project)
-        email.setValue(model.email)
-        confirmation.setVisible(model.showConfirmation)
-        failureNotification.setVisible(model.showFailure)
+    private void bindToModel() {
+        confirmationDisplay.setVisible(model.showConfirmation)
+        failureDisplay.setVisible(model.showFailure)
     }
 
-    void setListeners() {
+    private void setListeners() {
         model.addPropertyChangeListener("project", {
-            project.setValue(it.newValue as String)
+            confirmationDisplay.setProject(it.newValue as String)
         })
         model.addPropertyChangeListener("email", {
-            email.setValue(it.newValue as String)
+            confirmationDisplay.setEmail(it.newValue as String)
         })
         model.addPropertyChangeListener("showConfirmation", {
-            failureNotification.setVisible(!it.newValue as Boolean)
-            confirmation.setVisible(it.newValue as Boolean)
+            failureDisplay.setVisible(!it.newValue as Boolean)
+            confirmationDisplay.setVisible(it.newValue as Boolean)
         })
 
     }
 
-    void init() {
+    private void init() {
         createConfirmationLayout()
         createFailureNotificationLayout()
     }
 
-    void createConfirmationLayout() {
-        confirmation = new VerticalLayout()
-        Label affirmative = new Label(VaadinIcons.CHECK_CIRCLE_O.getHtml(), ContentMode.HTML)
-        affirmative.addStyleName(ValoTheme.LABEL_H1)
-        affirmative.addStyleName(ValoTheme.LABEL_COLORED)
-        affirmative.setSizeUndefined()
-        Label mainInfo = new Label("Subscription cancelled.")
-        mainInfo.addStyleName(ValoTheme.LABEL_H1)
-        mainInfo.addStyleName(ValoTheme.LABEL_COLORED)
-        mainInfo.setSizeUndefined()
-        Label label = new Label("We stop sending updates for")
-        label.setStyleName(ValoTheme.LABEL_SMALL)
-        label.setSizeUndefined()
-        project = new Label("QTEST")
-        project.addStyleName(ValoTheme.LABEL_H3)
-        project.setSizeUndefined()
-        Label toLabel = new Label("to")
-        toLabel.setStyleName(ValoTheme.LABEL_SMALL)
-        toLabel.setSizeUndefined()
-        email  = new Label("sven.fillinger@web.de")
-        email.setStyleName(ValoTheme.LABEL_H3)
-        email.setSizeUndefined()
-        confirmation.addComponent(affirmative)
-        confirmation.addComponent(mainInfo)
-        confirmation.addComponent(label)
-        confirmation.addComponent(project)
-        confirmation.addComponent(toLabel)
-        confirmation.addComponent(email)
-        confirmation.setComponentAlignment(affirmative, Alignment.MIDDLE_CENTER)
-        confirmation.setComponentAlignment(mainInfo, Alignment.MIDDLE_CENTER)
-        confirmation.setComponentAlignment(label, Alignment.MIDDLE_CENTER)
-        confirmation.setComponentAlignment(project, Alignment.MIDDLE_CENTER)
-        confirmation.setComponentAlignment(toLabel, Alignment.MIDDLE_CENTER)
-        confirmation.setComponentAlignment(email, Alignment.MIDDLE_CENTER)
-        confirmation.setSizeFull()
-        this.addComponent(confirmation)
+    private void createConfirmationLayout() {
+        this.confirmationDisplay = new ConfirmationDisplay()
+        this.addComponent(confirmationDisplay)
     }
 
-    void createFailureNotificationLayout() {
-        failureNotification = new VerticalLayout()
-        Label failure = new Label(VaadinIcons.CLOSE.getHtml(), ContentMode.HTML)
-        failure.addStyleName(ValoTheme.LABEL_H1)
-        failure.addStyleName("failure-red")
-        failure.setSizeUndefined()
-        Label mainInfo = new Label("Cancellation failed!")
-        mainInfo.addStyleName(ValoTheme.LABEL_H1)
-        mainInfo.addStyleName("failure-red")
-        mainInfo.setSizeUndefined()
-        Label label = new Label("You did not cancel you subscription.")
-        label.setStyleName(ValoTheme.LABEL_SMALL)
-        label.setSizeUndefined()
-        Label label2 = new Label("Please write an email to <a href='mailto:support@qbic.zendesk.com'>support@qbic.zendesk.com</a> for help.", ContentMode.HTML)
-        label.setStyleName(ValoTheme.LABEL_SMALL)
-        label.setSizeUndefined()
-        failureNotification.addComponent(failure)
-        failureNotification.addComponent(mainInfo)
-        failureNotification.addComponent(label)
-        failureNotification.addComponent(label2)
-        failureNotification.setComponentAlignment(failure, Alignment.MIDDLE_CENTER)
-        failureNotification.setComponentAlignment(mainInfo, Alignment.MIDDLE_CENTER)
-        failureNotification.setComponentAlignment(label, Alignment.MIDDLE_CENTER)
-        failureNotification.setComponentAlignment(label2, Alignment.MIDDLE_CENTER)
-        this.addComponent(failureNotification)
+    private void createFailureNotificationLayout() {
+        this.failureDisplay = new FailureDisplay()
+        this.addComponent(failureDisplay)
     }
 }
---
+++ subscription-cancel-app/src/main/groovy/life/qbic/portal/subscription/view/model/FailureDisplay.groovy
@@ -0,0 +1,46 @@
+package life.qbic.portal.subscription.view.model
+
+import com.vaadin.icons.VaadinIcons
+import com.vaadin.shared.ui.ContentMode
+import com.vaadin.ui.Alignment
+import com.vaadin.ui.Component
+import com.vaadin.ui.Label
+import com.vaadin.ui.VerticalLayout
+import com.vaadin.ui.themes.ValoTheme
+
+/**
+ * Display screen we want to show to the user, when the subscription cancellation has failed.
+ *
+ * @since 1.0.0
+ */
+class FailureDisplay extends VerticalLayout {
+
+    FailureDisplay() {
+        buildDisplay()
+    }
+
+    private void buildDisplay() {
+        Label failureIcon = new Label(VaadinIcons.CLOSE.getHtml(), ContentMode.HTML)
+        failureIcon.addStyleName(ValoTheme.LABEL_H1)
+        failureIcon.addStyleName("failure-red")
+
+        Label topic = new Label("Cancellation failed!")
+        topic.addStyleName(ValoTheme.LABEL_H1)
+        topic.addStyleName("failure-red")
+
+        Label failureNote = new Label("You did not cancel you subscription.")
+        failureNote.setStyleName(ValoTheme.LABEL_SMALL)
+
+        Label actionNote = new Label("Please write an email to <a href='mailto:support@qbic.zendesk.com'>support@qbic.zendesk.com</a> for help.", ContentMode.HTML)
+        actionNote.setStyleName(ValoTheme.LABEL_SMALL)
+
+        List<Component> components = [failureIcon, topic, failureNote, actionNote]
+        components.stream().forEach(c -> {
+            c.setSizeUndefined()
+            this.addComponent(c)
+            this.setComponentAlignment(c, Alignment.MIDDLE_CENTER)
+        })
+
+        this.setSizeFull()
+    }
+}
